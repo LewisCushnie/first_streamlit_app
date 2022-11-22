046596e606f67df2223333df6fd662fc62e41c0d
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-22 14:42:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import snowflake.connector
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
