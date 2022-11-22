003849cbf0269919d9c44e8db0ec52c11fefba9d
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-22 15:11:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ streamlit.write('The user entered ', add_my_fruit)
 #streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 
-streamlit.header("The fruit load list contains:)
+streamlit.header("The fruit load list contains:")
 # Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
