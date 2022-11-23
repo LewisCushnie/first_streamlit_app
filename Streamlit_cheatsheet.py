c6a47cbf8df36c2e60849a18cd319a041b22d1af
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-23 11:26:40 +0000

    adding side bars

--- Streamlit_cheatsheet.py
+++ Streamlit_cheatsheet.py
@@ -134,6 +134,21 @@ option = st.selectbox(
 
 'You selected: ', option
 
+# ----------------------------------------------------------------------------------
+# USING SIDEBARS
+# ----------------------------------------------------------------------------------
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
 
 
 st.stop()
