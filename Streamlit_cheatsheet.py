924eb3163c8a965569fa0e47d0c0f715a125cb8e
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-23 11:44:10 +0000

    another quick change

--- Streamlit_cheatsheet.py
+++ Streamlit_cheatsheet.py
@@ -178,11 +178,11 @@ st.header('Demo progress bar using time.sleep')
 latest_iteration = st.empty()
 bar = st.progress(0)
 
-for i in range(10):
+for i in range(100):
   # Update the progress bar with each iteration.
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)
-  time.sleep(0.1)
+  time.sleep(0.01)
 
 '...and now we\'re done!'
 
