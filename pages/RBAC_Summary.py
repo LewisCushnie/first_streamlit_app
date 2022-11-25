b18b5c6d3cc17aa9e45e7da45db29b6ee7bd22a0
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 16:22:14 +0000

    a

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -3,11 +3,11 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-from snowflake_demo import a
+from snowflake_demo import all_RBAC_roles
 
 
 st.title('RBAC Roles Summary')
-st.write(a)
+st.write(all_RBAC_roles)
 #st.dataframe(all_RBAC_roles)
 
 # Stop streamlit from running past this point
