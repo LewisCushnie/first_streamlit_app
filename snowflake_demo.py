3be185f174d9174a862e0b42287a8d3ed889160f
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 16:12:02 +0000

    undo

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -3,11 +3,27 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-from streamlit_functions import init_connection, run_query
 
 st.title('Snowflake Connectivity Demo')
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+# Initialize snowflake connection object
 conn = init_connection()
+
+# Funtion to perform queries from the database.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 all_RBAC_roles = run_query("select CREATED_ON, NAME, COMMENT, OWNER from roles;")
 st.header("Roles Summary:")
