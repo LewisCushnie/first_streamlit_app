50a2f6521cb3dfbd08e78bd7757ac90a80a2b43d
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 15:53:37 +0000

    a

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -8,7 +8,7 @@ import snowflake_demo as sd
 st.title('Resource Monitoring Summary')
 
 all_RBAC_roles = sd.run_query("select name, credits_used from metering_history;")
-st.header("Roles Summary:")
+st.header("Metering:")
 st.dataframe(all_RBAC_roles)
 
 # Stop streamlit from running past this point
