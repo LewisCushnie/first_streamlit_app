8612d446925bf9b4713902e3ea4fa7b63ba05be0
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 13:54:09 +0000

    un q

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -3,9 +3,10 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+import snowflake_demo as sd
 
 st.title('RBAC Roles Summary')
 
-all_RBAC_roles = run_query("select * from roles;")
+all_RBAC_roles = sd.run_query("select * from roles;")
 st.header("Roles Summary:")
 st.dataframe(all_RBAC_roles)
