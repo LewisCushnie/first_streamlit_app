178f652fb3a4e6a7a3baeadac3b988bc765e8682
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 12:03:03 +0000

    trying

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -1,50 +1,20 @@
-import streamlit
+import streamlit as st
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My parents healthy dinner')
+st.title('Snowflake Connectivity Demo')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado toast')
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from roles")
+my_data_rows = my_cur.fetchall()
+st.header("Roles:")
+st.dataframe(my_data_rows)
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-
-# New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to start")
-  else:
-    # streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # Normalise json response
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # Output to screen as table
-    streamlit.dataframe(fruityvice_normalized)
-
-except URLError as e:
-  streamlit.error()
-
-## Allow the end user to add a fruit to the list
-#add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', add_my_fruit)
-## my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# Stop streamlit from running past this point
+st.stop()
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
@@ -52,20 +22,13 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding" + new_fruit
     
-add_my_fruit = streamlit.text_input('What fruit would you like to add')
-if streamlit.button('Add a fruit to the list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+add_my_fruit = st.text_input('What fruit would you like to add')
+if st.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)
+  st.text(back_from_function)
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load list contains:")
-#streamlit.dataframe(my_data_rows)
-
-streamlit.header("The fruit load list contains:")
+st.header("The fruit load list contains:")
 # Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -73,13 +36,8 @@ def get_fruit_load_list():
     return my_cur.fetchall()
     
 # Add button to load fruit
-if streamlit.button('Get fruit load list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+if st.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
-  streamlit.dataframe(my_data_rows)
-  
-# Stop streamlit from running past this point
-streamlit.stop()
-
-# Test test test
+  st.dataframe(my_data_rows)
