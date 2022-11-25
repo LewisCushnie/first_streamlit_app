57d2d5f02394db0b67022d635a9d7e660fe0fe28
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 15:29:06 +0000

    a

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -3,29 +3,30 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
+import snowflake_demo as sd
 
 st.title('RBAC Roles Summary')
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
+# # Initialize connection.
+# # Uses st.experimental_singleton to only run once.
+# @st.experimental_singleton
+# def init_connection():
+#     return snowflake.connector.connect(
+#         **st.secrets["snowflake"], client_session_keep_alive=True
+#     )
 
-# Initialize snowflake connection object
-conn = init_connection()
+# # Initialize snowflake connection object
+# conn = init_connection()
 
-# Funtion to perform queries from the database.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# # Funtion to perform queries from the database.
+# # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+# @st.experimental_memo(ttl=600)
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
 
-all_RBAC_roles = run_query("select * from roles;")
+all_RBAC_roles = sd.run_query("select * from roles;")
 st.header("Roles Summary:")
 st.dataframe(all_RBAC_roles)
 
