1253acf41e7a6cb0d47b9a0f7889088795cf044f
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 13:56:41 +0000

    a

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -7,6 +7,28 @@ import snowflake_demo as sd
 
 st.title('RBAC Roles Summary')
 
-all_RBAC_roles = sd.run_query("select * from roles;")
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+# Initialize snowflake connection object
+conn = init_connection()
+
+# Funtion to perform queries from the database.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+# Stop streamlit from running past this point
+st.stop()
+
+all_RBAC_roles = run_query("select * from roles;")
 st.header("Roles Summary:")
 st.dataframe(all_RBAC_roles)
