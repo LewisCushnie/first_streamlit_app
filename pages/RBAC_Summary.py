da4b7c72126788b949146e60d1140159c87f201d
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-25 16:19:29 +0000

    a

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -3,12 +3,9 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-from snowflake_demo import run_query
+from snowflake_demo import all_RBAC_roles
 
 st.title('RBAC Roles Summary')
-
-all_RBAC_roles = run_query("select * from roles;")
-st.header("Roles Summary:")
 st.dataframe(all_RBAC_roles)
 
 # Stop streamlit from running past this point
