b1749276e90ad5fa01077c03fba9ab9e34c9c9fa
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-28 21:29:20 +0000

    a

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -21,7 +21,7 @@ conn = init_connection()
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
-        return cur.fetchall()
+        return cur.fetch_pandas_all()
 
 #============================= PAGE STARTS =================================
 
