bced3cb8c26b285924abb97a3047be25d0318e73
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-11-28 20:35:26 +0000

    a

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -32,8 +32,8 @@ metering = run_query("select name, credits_used from metering_history;")
 metering_df = pd.DataFrame(metering, columns=['Name', 'Credits Used'])
 
 # Get top 10 warehouses credit usage
-# metering_top_10 = run_query("select top 10 name, credits_used from metering_history;")
-# metering_top_10_df = pd.DataFrame(metering, columns=['Name', 'Credits Used'])
+metering_top_10 = run_query("select top 10 name, credits_used from metering_history;")
+metering_top_10_df = pd.DataFrame(metering, columns=['Name', 'Credits Used'])
 
 # Display the filtered df on the page
 st.dataframe(metering_df, width=500)
