d796d646c3d81461648e8305bf1d2abf5ff12487
LewisCushnie <lewiscushnie@icloud.com>
2022-11-30 08:24:12 +0000

    a

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -33,7 +33,7 @@ metering_top_10 = run_query("select top 10 name, sum(credits_used) from metering
 metering_top_10_df = pd.DataFrame(metering_top_10, columns=['index', 'Credits Used'])
 st.write(metering_top_10_df)
 st.dataframe(metering_top_10_df)
-st.bar_chart(metering_top_10_df)
+st.bar_chart(metering_top_10_df['index', 'Credit Used'])
 st.stop()
 
 # Get top 10 warehouses credit usage
