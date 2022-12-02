b052b9f2eff94ee095786744eb903eed598a58d9
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-02 16:12:40 +0000

    test

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -33,6 +33,7 @@ st.title('Resource Monitoring Summary')
 metering_top_10 = run_query("select top 10 name, sum(credits_used) from metering_history group by name;")
 metering_top_10_df = pd.DataFrame(metering_top_10, columns=['X', 'Y'])
 metering_top_10_df = metering_top_10_df.set_index('X')
+metering_top_10_df['Y'] = metering_top_10_df['Y'].astype(float)
 st.write(metering_top_10_df)
 st.bar_chart(metering_top_10_df)
 
