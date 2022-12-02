3ad2477a562de214a26cd1289503eeefd9f43cae
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-02 15:57:23 +0000

    test

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -3,25 +3,26 @@ import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
+from snowflake_demo import run_query
 
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return snowflake.connector.connect(
-        **st.secrets["snowflake"], client_session_keep_alive=True
-    )
+# # Initialize connection.
+# # Uses st.experimental_singleton to only run once.
+# @st.experimental_singleton
+# def init_connection():
+#     return snowflake.connector.connect(
+#         **st.secrets["snowflake"], client_session_keep_alive=True
+#     )
 
-# Initialize snowflake connection object
-conn = init_connection()
+# # Initialize snowflake connection object
+# conn = init_connection()
 
-# Funtion to perform queries from the database.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+# # Funtion to perform queries from the database.
+# # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+# @st.experimental_memo(ttl=600)
+# def run_query(query):
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         return cur.fetchall()
 
 #============================= PAGE STARTS =================================
 
