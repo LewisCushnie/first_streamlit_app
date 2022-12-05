852eb141fb2ed21539d3607011ff0f3089c4b777
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-05 20:21:54 +0000

    test added

---
+++ pages/test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+import numpy as np
+from urllib.error import URLError
+import altair as alt
+from snowflake_demo import run_query, init_connection
+
+conn = init_connection()
+
+st.title('Snowflake Connectivity Demo')
+all_RBAC_roles = run_query("select CREATED_ON, NAME, COMMENT, OWNER from roles;")
+st.header("Roles Summary:")
+st.dataframe(all_RBAC_roles)
+
