83ab7e2a13a3515c642666bf62e967ce2f40c51e
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-05 21:46:41 +0000

    a

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -9,19 +9,40 @@ conn = init_connection()
 
 #============================= PAGE STARTS =================================
 
+#------------------------------- SIDEBAR ----------------------------------- 
+st.sidebar.header('Snowflake session')
+
 streamlit_credits_used = run_query(
 '''select
 sum(credits_used_cloud_services)
 from query_history
 where query_tag = 'StreamlitQuery';'''
 )
-streamlit_credits_used_df = pd.DataFrame(streamlit_credits_used, columns=['Streamlit_Credits_Used'])
-st.write(streamlit_credits_used_df)
 
-st.sidebar.header('Credits used in streamlit')
+snowflake_session_variables = run_query(
+'''select current_database() 
+,current_schema()
+,current_role()
+,current_session()
+,current_user()
+,current_warehouse()
+,current_region()
+,current_time();'''
+)
+
+streamlit_credits_used_df = pd.DataFrame(streamlit_credits_used, columns=['Streamlit_Credits_Used'])
 credits = streamlit_credits_used_df.iloc[0]['Streamlit_Credits_Used']
 rounded_credits = round(credits, 5)
 st.sidebar.metric("Credits used", rounded_credits)
+
+snowflake_session_variables_df = pd.DataFrame(snowflake_session_variables, 
+columns=['Database', 'Schema', 'Role', 'Session', 'User', 'Warehouse', 'Region', 'Time'])
+
+sf.write(snowflake_session_variables_df)
+
+
+
+#------------------------------- SIDEBAR ----------------------------------- 
 
 st.title('Snowflake Connectivity Demo')
 
