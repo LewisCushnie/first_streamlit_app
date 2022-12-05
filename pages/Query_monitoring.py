276c20cf35415fee0d228efa7a9ff65d99a36efb
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-05 17:34:52 +0000

    a

--- pages/Query_monitoring.py
+++ pages/Query_monitoring.py
@@ -50,18 +50,14 @@ st.header('most recent queries')
 most_recent_queries = run_query(
 '''
 select top 10 query_id
-,credits_used_cloud_services
-,total_elapsed_time/60000 as minutes_to_complete
-,query_type
-,database_name
+,query_text
+,start_time
 ,query_tag
 from SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY order by start_time desc;'''
 )
 
 # Convert to pandas dataframe
-most_expensive_queries_df = pd.DataFrame(most_recent_queries, columns=['QUERY_ID','CREDITS_USED_CLOUD_SERVICES','MINUTES_TO_COMPLETE', 'QUERY_TYPE', 'DATABASE_NAME', 'QUERY_TAG'])
+most_expensive_queries_df = pd.DataFrame(most_recent_queries, columns=['id','text','time', 'tag'])
 #most_expensive_queries_df = most_expensive_queries_df.set_index('CREDITS_USED_CLOUD_SERVICES')
-most_expensive_queries_df['CREDITS_USED_CLOUD_SERVICES'] = most_expensive_queries_df['CREDITS_USED_CLOUD_SERVICES'].astype(float)
-most_expensive_queries_df['MINUTES_TO_COMPLETE'] = most_expensive_queries_df['MINUTES_TO_COMPLETE'].astype(float)
 st.header("Queries Summary:")
 st.dataframe(most_expensive_queries_df)
