d8b0a1e54555784f1fa7da9d4abf297df15a7491
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-05 21:31:05 +0000

    a

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -20,7 +20,8 @@ st.write(streamlit_credits_used_df)
 
 st.sidebar.header('Credits used in streamlit')
 credits = streamlit_credits_used_df.iloc[0]['Streamlit_Credits_Used']
-st.sidebar.metric("Credits used", credits)
+rounded_credits = round(credits, 3)
+st.sidebar.metric("Credits used", rounded_credits)
 
 st.title('Snowflake Connectivity Demo')
 
