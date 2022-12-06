9d7b9ebb66729d6264cecb6fd4fd7b41175be815
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-06 10:04:03 +0000

    a

--- snowflake_demo.py
+++ snowflake_demo.py
@@ -40,8 +40,8 @@ snowflake_session_variables_df = pd.DataFrame(snowflake_session_variables,
 columns=['Database', 'Schema', 'Role', 'Session', 'User', 'Warehouse', 'Region', 'Time'])
 transposed_session_variables_df = snowflake_session_variables_df.transpose().reset_index()
 transposed_session_variables_df = transposed_session_variables_df.rename(columns={"index": "Session Parameter", 0: "Value"})
-transposed_session_variables_df = transposed_session_variables_df.set_index('Session Parameter')
-st.sidebar.dataframe(transposed_session_variables_df, use_container_width= True)
+#transposed_session_variables_df = transposed_session_variables_df.set_index('Session Parameter')
+st.sidebar.dataframe(transposed_session_variables_df)
 
 #------------------------------- SIDEBAR ----------------------------------- 
 
